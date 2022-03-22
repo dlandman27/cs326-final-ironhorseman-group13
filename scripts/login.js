72d8c6b0bb2b919656dd0d5d3aa475d0e53bb3a7
Dylan Landman <dlandman@umass.edu>
2022-03-22 15:23:47 -0400

    Reshaped home page

---
+++ scripts/login.js
@@ -0,0 +1,27 @@
+// JS USED IN THE LOGIN/SIGNUP PAGE
+
+
+// Randomly generates hearts club diamonds and spades
+function dropdown_images(){
+    const dropdown = setInterval(function(){
+        let images = ["hearts","clubs","diamonds","spades"];
+        let code = ["&#9829;","&#9827;","&#9830;","&#9824;"];
+        let colors = ["maroon","#36454F"];
+        let random_image = images[Math.floor(Math.random() * images.length)];
+        let dropdown_item = document.createElement("div");
+        dropdown_item.innerHTML = code[images.indexOf(random_image)];
+        dropdown_item.style.color = colors[Math.floor(Math.random() * colors.length)];
+        dropdown_item.style.position = "absolute";
+        dropdown_item.style.left = Math.floor(Math.random() * window.innerWidth) + "px";
+        dropdown_item.style.fontSize = "100px";
+        dropdown_item.style.userSelect = "none";
+        dropdown_item.style.zIndex = "-1";
+        dropdown_item.style.userSelect = "none";
+        dropdown_item.style.opacity = Math.random();
+        dropdown_item.style.animation = "move_suite_to_bottom 20s ease-in-out infinite";
+        document.body.append(dropdown_item);
+        setTimeout(function(){dropdown_item.remove()},20000);
+    },Math.floor(Math.random() * 300) + 1000);
+}
+
+dropdown_images();
