f1fd6e94dc936d055104b1b3417a91855bdf4ba5
Dylan Landman <dlandman@umass.edu>
2022-04-20 15:45:53 -0400

    Added some new express methods

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import * as http from 'http';
 import * as url from 'url';
 import express from 'express';
+import { readFile, writeFile } from 'fs/promises';
+import logger from 'morgan';
 
 let knownUsers = [];
 
@@ -8,7 +10,14 @@ let knownUsers = [];
 let usersFile = 'UsersFile.json';
 //reloads
 const app = express();
-app.use(express.static('public'));
+const port = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use('/client', express.static('client'));
 
 async function reloadUsers() {
   try {
@@ -87,35 +96,51 @@ async function destroyUserFunc(response,name){
 }
 
 
-async function basicServer(request, response) {
-  // TODO: Implement the server
-  let theUrl = url.parse(request.url,true);
-  let queryVar = theUrl.query;
-  let pathVar = theUrl.pathname;
-  let methodVar = request.method;
+// async function basicServer(request, response) {
+//   // TODO: Implement the server
+//   let theUrl = url.parse(request.url,true);
+//   let queryVar = theUrl.query;
+//   let pathVar = theUrl.pathname;
+//   let methodVar = request.method;
   
 
-  if(methodVar == 'POST' && pathVar.startsWith('/addUser')){
-    addUserFunc(response,queryVar.name,queryVar.cash,queryVar.faction,queryVar.password);
-  }
-  else if (methodVar == 'GET' && pathVar.startsWith('/getUser')){
-    findUserFunc(response);
-  }
-  else if (methodVar == 'PATCH' && pathVar.startsWith('/updateUser')){
-    updateUserFunc(response,response,queryVar.name,queryVar.cash,queryVar.faction,queryVar.password);
-  }
-  else if (methodVar == 'DELETE' && pathVar.startsWith('/deleteUser')){
-    gameScoreFunc(response,queryVar.name,queryVar.score);
-  }
-  else{
-    response.writeHead(404, { 'Content-Type': 'text/plain' });
-    response.write(JSON.stringify({ message: 'Failure' }));
-    response.end();
-  }
-}
+//   if(methodVar == 'POST' && pathVar.startsWith('/addUser')){
+//     addUserFunc(response,queryVar.name,queryVar.cash,queryVar.faction,queryVar.password);
+//   }
+
+//   else if (methodVar == 'GET' && pathVar.startsWith('/getUser')){
+//     findUserFunc(response);
+//   }
+//   else if (methodVar == 'PATCH' && pathVar.startsWith('/updateUser')){
+//     updateUserFunc(response,response,queryVar.name,queryVar.cash,queryVar.faction,queryVar.password);
+//   }
+//   else if (methodVar == 'DELETE' && pathVar.startsWith('/deleteUser')){
+//     gameScoreFunc(response,queryVar.name,queryVar.score);
+//   }
+//   else{
+//     response.writeHead(404, { 'Content-Type': 'text/plain' });
+//     response.write(JSON.stringify({ message: 'Failure' }));
+//     response.end();
+//   }
+// }
+
+// // Start the server on port 3000.
+// http.createServer(basicServer).listen(3000, () => {
+//   console.log('Server started on port 3000');
+// });
 
 
-// Start the server on port 3000.
-http.createServer(basicServer).listen(3000, () => {
-  console.log('Server started on port 3000');
+app.post('/addUser', async (request, response) => {
+  const queryVar = request.query;
+  addUserFunc(response,queryVar.name,queryVar.cash,queryVar.faction,queryVar.password);
+});
+
+// This matches all routes that are not defined.
+app.all('*', async (request, response) => {
+  response.status(404).send(`Not found: ${request.path}`);
+});
+
+// Start the server.
+app.listen(port, () => {
+  console.log(`Server started on http://localhost:${port}`);
 });
