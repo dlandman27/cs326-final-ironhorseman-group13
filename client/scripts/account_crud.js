318bf98e0bfb74a77df4c7d0c75c1f5d86dfc039
TheSavant1912 <jstarman@umass.edu>
2022-05-07 16:54:11 -0400

    f

--- client/scripts/account_crud.js
+++ client/scripts/account_crud.js
@@ -50,7 +50,7 @@ async function updatePerson(username, password) {
 
 document.getElementById("change_pwd_button").addEventListener("click", async () => {
     
-    const person = await updatePerson(window.localStorage.getItem("username"), document.gerElementById("change_password").value);
+    const person = await updatePerson(window.localStorage.getItem("username"), document.getElementById("change_password").value);
     output.innerHTML = JSON.stringify(person);
     document.getElementById("account_info").innerHTML = "got the following:<br>" + responseText;
 });
